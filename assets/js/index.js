1cf8a883a206cd98bd7e85a62d823c83d6d17698
Julio-Valderrama <j.valdemartinez@gmail.com>
2022-06-08 15:33:27 +1000

    $Resource getUser() DONE !

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,58 @@
 // Create angular myApp
 
-angular.module('myApp', [])
+angular.module('myApp', ['ngResource'])
 
 angular.module("myApp")
 
+    .config(['$resourceProvider', function($resourceProvider){
+        $resourceProvider.defaults.stripTrailingSlashes = false;
+    }])
 
-    .controller("homeController", ["$scope", function($scope) {
+    .controller("homeController", ["$scope", "$resource", function($scope, $resource) {
 
         $scope.angularusers = window.angularusers;
         $scope.angularClick;
+        $scope.getCarResponse;
+
+        $scope.getUser = function() {
+
+            var getUser = $resource("/api/users?id=:id", {
+                id: "@id"
+            }, {
+                "typeReq": {
+                    method: "get",
+                    isArray: false
+                }
+            })
+            .typeReq({
+                id: "62a0036616a073a00adb26e5"
+            })
+            .$promise
+            .then(function(result) {
+                if (result) {
+                    $scope.getCarSucceeded = true;
+                    $scope.getCarResponse = JSON.stringify(result, null, 3);
+                 } else {
+                    $scope.getCarFailed = true;
+                    $scope.getCarErrorMsg = "Error occurred.";                  
+                 }
+                }, function(error) {
+                 $scope.getCarFailed = true;
+                 $scope.getCarErrorMsg = "Error occurred.";
+            });
+        }
 
         $scope.getDetails = (index) => {
             
             $scope.index = index + 1;
             $scope.angularClick = $scope.angularusers[index]
         }
+
+
+
+
+
+
     }])
 
 
